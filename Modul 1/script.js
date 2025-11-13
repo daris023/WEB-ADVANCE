93b4fe2056370ec1f2cabeff3641f493707046bd
daris023 <daris4721@gmail.com>
2025-11-13 20:37:09 +0100

    Add files via upload

---
+++ Modul 1/script.js
@@ -0,0 +1,104 @@
+console.log("goodbye");
+
+var x = 10;
+var value = "Digital School";
+
+
+var a = 50 + 50;
+console.log(a); 
+
+var b = 20;
+var c = 30;
+var d = b + c;
+console.log(d);   
+
+var f = 40;
+var g = 10;
+var h = f - g;
+console.log(h);
+
+var h= 100;
+var i= 5;
+var j= h / i;
+console.log(j);
+
+var k = 10;
+var l = 4;
+var m = k % l;
+console.log(m);
+
+var n = 99; 
+n++;
+console.log(n);
+
+var o = 101;
+o--;
+console.log(o);
+
+var p = 20;
+var q = p ** 2;
+console.log(q);
+
+var x =15;
+x += 5;
+console.log(x);
+
+var r =35;
+r -= 5;
+console.log(r);
+
+var s = 10;
+s *= 3;
+console.log(s);
+
+var t = 50;
+t /= 2;
+console.log(t);
+
+var u = 25;
+u %= 4;
+console.log(u);
+
+var v = 5;
+var w = 5;
+console.log(v == w);
+
+var y = 6;
+var z = 7;
+console.log(y != z);
+
+var num1 = 10;
+var num2 = 20;
+console.log(num1 < num2);
+
+var num3 = 30;
+var num4 = 25;
+console.log(num3 > num4);
+
+var num5 = 15;
+var num6 = 15;
+console.log(num5 <= num6);
+
+var num7 = 40;
+var num8 = 50;
+console.log(num7 === num8);  
+
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+var school = "Digital School";
+console.log("Hello from " + school);
+
+var age = 18;
+console.log(age >= 18 && age < 65);
+
+var var1 = 10;
+var var2 = 5;
+console.log(var1 == 5 || var2 == 3);
+
+var var3 = 5;
+var var4 = 10;
+console.log(!(var3 == var4));
